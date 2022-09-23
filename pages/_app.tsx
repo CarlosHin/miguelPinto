454294db27dd38401da3908e9e87284b56610962
Carlos Hinojosa <carloshin98@gmail.com>
2022-09-23 19:55:29 +0200

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/animate.min.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Miguel Pinto</title>
+        <meta name='description' content='Página oficial de Miguel Pinto Gallego' />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
